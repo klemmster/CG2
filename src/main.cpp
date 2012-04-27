f1f78fb441640a77910832d1670f82a2cb7f0a92
Richard Klemm <richy@coding-reality.de>
2012-04-28 00:05:04 +0200

    Dynamic file loading

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,23 @@
 #include <QtOpenGL/QGLWidget>
 #include "GLWidget.hpp"
 
+#include "offLoader.hpp"
+#include "vertex.hpp"
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    OffLoader loader;
+
+    std::string fileName;
+    if(argc > 1){
+        fileName = argv[1];
+    }else{
+        fileName = "torus.off";
+    }
+    VertexList vertices = loader.readOff(fileName);
     GLWidget window;
     window.resize(800,600);
     window.show();
