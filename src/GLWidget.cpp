869118f0f02837dabad238f2e8ab1f4b7666b3c8
Richard Klemm <Richy@coding-reality.de>
2012-05-29 00:36:22 +0200

    Wendland Approximation for Franke4.off

--- src/GLWidget.cpp
+++ src/GLWidget.cpp
@@ -51,20 +51,20 @@ void GLWidget::initializeGL() {
     glClearColor(0, 0, 0, 0);
 
 //    // Somewhere in the initialization part of your programâ¦
-//    glEnable(GL_LIGHTING);
-//    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
 //
 //    // Create light components
-//    GLfloat ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-//    GLfloat diffuseLight[] = { 0.8f, 0.8f, 0.8, 1.0f };
-//    GLfloat specularLight[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-//    GLfloat position[] = { -1.5f, 1.0f, -4.0f, 1.0f };
+    GLfloat ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+    GLfloat diffuseLight[] = { 0.8f, 0.8f, 0.8, 1.0f };
+    GLfloat specularLight[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+    GLfloat position[] = { -1.5f, 1.0f, -4.0f, 1.0f };
 //
 //    // Assign created components to GL_LIGHT0
-//    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
-//    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
-//    glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
-//    glLightfv(GL_LIGHT0, GL_POSITION, position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
 
     OffLoader loader;
     Stopwatch readTimer("ParseFile");
