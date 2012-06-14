19a9988e50b13d24780c92960589ac4bd87b6f80
Richard Klemm <richy@coding-reality.de>
2012-06-14 15:33:17 +0200

    use snail as standard mesh

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     if(argc > 1){
         fileName = argv[1];
     }else{
-        fileName = "torus.off";
+        fileName = "snail.off";
     }
 
     ui.glWidget->setFilename(fileName);
